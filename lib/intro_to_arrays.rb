10ac29f76a5af462343cac09eb5ceec3a7320347
The Natural <philip.smalls86@gmail.com>
2019-05-19 20:11:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,23 @@
+def instantiate_new_array
+end
+
+def array_with_two_elements
+end
+
+def first_element
+end
+
+def third_element
+end
+
+def last_element
+end
+
+def first_element_with_array_methods
+end
+
+def last_element_with_array_methods
+end
+
+def length_of_array
+end
